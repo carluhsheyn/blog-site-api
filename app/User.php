26412004bcd26d203eb7c7b5f8b94c92ef4d4a5b
karlashane <shane.srintern@gmail.com>
2019-11-06 19:45:42 +0800

    Created user login and register

--- app/User.php
+++ app/User.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -25,15 +25,16 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password'
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTIdentifier()
+   {
+       return $this->getKey();
+   }
+
+   public function getJWTCustomClaims()
+   {
+       return [];
+   }
 }
